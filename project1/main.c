851a2a3491d3474d72de3a35354aff88b46268d2
Caleb Davenport <caleb@calebdavenport.com>
2019-02-13 14:14:38 -0500

    Add hashing subfunctions

--- project1/main.c
+++ project1/main.c
@@ -8,34 +8,37 @@ long fsize(const char *filename);
 long messageBlocks(off_t message);
 
 int main() {
+    char eof = 0;
 	FILE *fp;
     char block[1024];
+    char buffer[128];
+    unsigned long long currentHash[8];
 
-    memset(block, 'A', sizeof block);
+    for (int i = 0; i < 8; i++) {
+        currentHash[i] = initialHash[i];
+        printf("%llx\n", currentHash[i]);
+    }
 
-	fp = fopen("test.txt", "r");
-	// fprintf(fp, "This is testing for fprintf...\n");
-	// fputs("This is testing for fputs...\n", fp);
-	fclose(fp);
+	fp = fopen("test.txt", "rb");
 	printf("TEST\n");
     printf("0x%lx\n", fsize("test.txt"));
-    printf("0x%lx\n", messageBlocks(128));
-
-/*
-	for (int i = 0; i < 128; ++i)
-	{
-		block[i] = 'A';
-	}
-*/
-    for (int i = 0; i < 8; i++) {
-        // printf("%c", block[i]);
-        for (int j = 0; j < 128; j++) {
-            int index = 128 * i + j;
-            printf("%c", block[index]);
+    printf("0x%lx\n", messageBlocks(111));
+    if ( fp != NULL ) {
+        int read = 0;
+        while((read = fread(buffer, 1, 128, fp)) > 0) {
+            for(int i = 0; i < read; i++) {
+                if (i > 111) {
+                    ; // If the message ends above this range, another
+                      // block must be made
+                }
+                printf("%c", buffer[i]);
+            }
+            printf("\n");
+            printf("%x", *buffer);
+            printf("\n---------\n");
         }
-        printf("\n");
     }
-	return 0;
+    fclose(fp);
 
 }
 
@@ -58,3 +61,30 @@ long messageBlocks(off_t message) {
     return (totalSize / 1024) + 1;
 }
 
+long long rightRot(long long x, unsigned int d) {
+    return (x >> d) | (x << (64 - d));
+}
+
+long long ch_function(long long x, long long y, long long z) {
+    return (x & y) ^ (~x & z);
+}
+
+long long maj_function(long long x, long long y, long long z) {
+    return (x & y) ^ (x & z) ^ (y & z);
+}
+
+long long sigma_0(long long x) {
+    return rightRot(x, 28) ^ rightRot(x, 34) ^ rightRot(x, 39);
+}
+
+long long sigma_1(long long x) {
+    return rightRot(x, 14) ^ rightRot(x, 18) ^ rightRot(x, 41);
+}
+
+long long delta_0(long long x) {
+    return rightRot(x, 1) ^ rightRot(x, 8) ^ (x >> 7);
+}
+
+long long delta_1(long long x) {
+    return rightRot(x, 19) ^ rightRot(x, 61) ^ (x >> 6);
+}
